39941ee2a7061825433678f1864e02f9a7554c17
Aran Leite <hyoretsu@gmail.com>
2021-09-23 18:28:43 -0300

    Added validation check for moves

--- src/main/java/com/hyoretsu/checkers/Board.java
+++ src/main/java/com/hyoretsu/checkers/Board.java
@@ -22,4 +22,28 @@ public class Board {
  public Square getSquare(Integer x, Integer y) {
   return this.squares[x][y];
  }
+
+ /** Checks if the given move is valid. */
+ public Boolean validMove(Square origin, Square destination) {
+  if (destination.hasPiece()) {
+   return false;
+  }
+
+  // Within 1 square
+  Boolean validX = Math.abs(origin.getX() - destination.getX()) == 1;
+
+  // Moving forward
+  Boolean validY;
+  if (origin.getPiece().getColor() == Piece.WHITE) {
+   validY = destination.getY() == origin.getY() - 1;
+  } else {
+   validY = destination.getY() == origin.getY() + 1;
+  }
+
+  if (!validX || !validY) {
+   return false;
+  }
+
+  return true;
+ }
 }
